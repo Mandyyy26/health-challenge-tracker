93207ce9e3cc87682aefd6c1cb8f3b4f6a7b5f1e
Mandeep Malik <mandeepmalik2003@gmail.com>
2025-02-03 01:25:47 +0530

    initialised components and services

---
+++ src/app/workout-search/workout-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-search.component.html',
+  styleUrl: './workout-search.component.scss'
+})
+export class WorkoutSearchComponent {
+
+}
